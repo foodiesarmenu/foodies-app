ac2db88081401cf638d2713b53230fa8c2799342
Yehya403 <yehyagamal95@gmail.com>
2024-03-23 05:14:45 +0200

    + Fixed registration bug

--- lib/data/model/request/RegisterRequest.dart
+++ lib/data/model/request/RegisterRequest.dart
@@ -10,40 +10,28 @@ class RegisterRequest {
   RegisterRequest({
     this.name,
     this.email,
-    this.countryCode,
     this.phoneNumber,
     this.password,
-    this.gender,
-    this.dateOfBirth,
   });
 
   RegisterRequest.fromJson(dynamic json) {
     name = json['name'];
     email = json['email'];
-    countryCode = json['countryCode'];
     phoneNumber = json['phoneNumber'];
     password = json['password'];
-    gender = json['gender'];
-    dateOfBirth = json['dateOfBirth'];
   }
 
   String? name;
   String? email;
-  String? countryCode;
   String? phoneNumber;
   String? password;
-  String? gender;
-  String? dateOfBirth;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['name'] = name;
     map['email'] = email;
-    map['countryCode'] = countryCode;
     map['phoneNumber'] = phoneNumber;
     map['password'] = password;
-    map['gender'] = gender;
-    map['dateOfBirth'] = dateOfBirth;
     return map;
   }
 }
