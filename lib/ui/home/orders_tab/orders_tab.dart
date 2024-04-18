1755dc4124d07fa406ed6b2113e37ae20ca2f9ef
Mahmoud Hossam <79559477+mahmoudhossammohamed@users.noreply.github.com>
2024-04-18 04:31:36 +0200

    orders_tab UI

--- lib/ui/home/orders_tab/orders_tab.dart
+++ lib/ui/home/orders_tab/orders_tab.dart
@@ -1,18 +1,145 @@
 import 'package:flutter/material.dart';
 
+import '../../common/ButtonInProfile.dart';
+import '../../common/custom_app_bar.dart';
+import 'order_list.dart';
+
 class OrdersTab extends StatelessWidget {
   static const String routeName = 'OrdersSc';
 
-  const OrdersTab({super.key});
+  OrdersTab({super.key});
+
+  final List<Order> orders = [
+    Order(
+      id: 'ss',
+      restaurantName: 'Restaurant Name',
+      price: 'EGP 100',
+      orderStatus: 'Delivered',
+      orderDate: 'April 15, 2024',
+      imageUrl: 'assets/images/7oda.png',
+    ),
+    Order(
+      id: 'ss',
+      restaurantName: 'Restaurant Name',
+      price: 'EGP 250',
+      orderStatus: 'Pending',
+      orderDate: 'April 17, 2024',
+      imageUrl: 'assets/images/7oda.png',
+    ),
+    Order(
+      id: 'ss',
+      restaurantName: 'Restaurant Name',
+      price: 'EGP 700',
+      orderStatus: 'Canceled',
+      orderDate: 'April 19, 2024',
+      imageUrl: 'assets/images/7oda.png',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: CircularProgressIndicator()),
-        ],
+    return Scaffold(
+      appBar: CustomAppBar.buildAppBar(context, 'Orders'),
+      body: ListView.separated(
+        separatorBuilder: (context, index) {
+          return Divider(indent: 30,endIndent: 30,);
+        },
+        itemCount: orders.length, // Length of the orders list
+        itemBuilder: (context, index) {
+          final order = orders[index]; // Get the current order
+          Color statusColor;
+          switch (order.orderStatus.toLowerCase()) {
+            case 'delivered':
+              statusColor = Colors.green;
+              break;
+            case 'pending':
+              statusColor = Theme.of(context).primaryColor;
+              break;
+            case 'canceled':
+              statusColor = Colors.red;
+              break;
+            default:
+              statusColor = Colors.black;
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+            child: Column(
+               children: [
+                 Row(
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 3,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(order.imageUrl), // Use image URL from the order
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16,),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(order.restaurantName,
+                                  style: Theme.of(context).textTheme.titleSmall
+                                ),
+                              ),
+                              Text(order.orderStatus,
+                                  style: Theme.of(context).textTheme.bodySmall?.copyWith(color: statusColor)
+                              ),
+                            ],
+                          ),
+                          Text(order.orderDate,
+                            style: Theme.of(context).textTheme.bodySmall
+                          ),
+                          //SizedBox(height: 16,),
+                          Text(order.price,
+                            style: Theme.of(context).textTheme.titleSmall
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+            ),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   children: [
+                     ButtonInProfile(
+                       onPressed: (){},
+                       backgroundColor: Theme.of(context).primaryColor,
+                       text: 'reorder',
+                       textColor: Colors.white,
+                       width: 150,
+                       icon: Icons.refresh_outlined,
+                       height: 40,
+                     ),
+                     ButtonInProfile(
+                       onPressed: (){},
+                       backgroundColor: Colors.white,
+                       text: 'feedback',
+                       textColor: Theme.of(context).primaryColor,
+                       width: 150,
+                       borderColor: Theme.of(context).primaryColor,
+                       icon: Icons.feedback_outlined,
+                       height: 40,
+                     ),
+                   ],
+                 )
+               ],
+             ),
+          );
+        },
+        //
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import connectivity_plus
 import geolocator_apple
-import location
 import network_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -16,7 +15,6 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
