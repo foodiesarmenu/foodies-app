d4d10f63b0df291fc38fe7f573fe2e328cd26e95
Yehya403 <yehyagamal95@gmail.com>
2024-05-17 01:28:53 +0300

    +

--- lib/domain/usecase/get_promotions_use_case.dart
+++ lib/domain/usecase/get_promotions_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:foodies_app/domain/failures.dart';
 import 'package:injectable/injectable.dart';
+
 import '../model/Promotion.dart';
 import '../repositoryContract/home_repository.dart';
 
